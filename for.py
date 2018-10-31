0217354c17617945e4920f69b43e5779f2668c4b
OSampson21 <44277509+OSampson21@users.noreply.github.com>
2018-10-31 09:50:55 -0500

    Create for.py

---
+++ for.py
@@ -0,0 +1,18 @@
+import RoboPiLip as RPL
+import time
+
+RPL.RoboPiInit("/dev/ttyAMA0",115200)
+list = [1,2,3]
+motorR = 0
+motorL = 1
+AnalogSensor = 3
+sensorF = 17
+sensorL = 16
+
+while True:
+  readingF = RPL.digitalRead(senorF
+  if readingF == 0:
+     for number in list
+       return 5
+       
+  
