47b56ef1efd1e1410c6d058bc1d91c0d746d7d42
OSampson21 <44277509+OSampson21@users.noreply.github.com>
2018-10-31 09:26:35 -0500

    Create if.py

---
+++ if.py
@@ -0,0 +1,17 @@
+import RoboPiLip as RPL
+import time
+
+RPL.RoboPiInit("/dev/ttyAMA0",115200)
+
+motorR = 0
+motorL = 1
+AnalogSensor = 3
+sensorF = 17
+sensorS = 16
+
+while True:
+  readingS = RPL.digitalRead(sensorS)
+  if readingS == 0:
+     RPL.servoWrite(motorR, 1900)
+  else:
+     RPL.servoWrite(motorR, 0)
