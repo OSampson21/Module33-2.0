62fa34721442f3d7dda7e707b205f9aa894248ec
OSampson21 <44277509+OSampson21@users.noreply.github.com>
2018-10-31 09:43:50 -0500

    Create sleep.py

---
+++ sleep.py
@@ -0,0 +1,22 @@
+import RoboPiLip as RPL
+import time
+
+RPL.RoboPiInit("/dev/ttyAMA0",115200)
+
+motorR = 0
+motorL = 1
+AnalogSensor = 3
+sensorF = 17
+sensorL = 16
+
+while True:
+  readingF = RPL.digitalRead(senorF)
+  readingL = RPL.digitalRead(sensorL)
+  if readingL == 0:
+     RPL.servoWrite(motorL, 1600)
+     time.sleep(1)
+  else:
+     break
+     
+RPL.servoWrite(motorR, 0)
+
