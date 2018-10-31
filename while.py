b6cd305d648ec655b244b079bfdc55b48e4c314d
OSampson21 <44277509+OSampson21@users.noreply.github.com>
2018-10-31 10:01:08 -0500

    Create while.py

---
+++ while.py
@@ -0,0 +1,24 @@
+import RoboPiLib as RPL
+import time
+
+RPL.RoboPiInit("/dev/ttyAMA0",115200)
+list = [1,2,3]
+motorR = 0
+motorL = 1
+theB = time.time()
+sensorF = 17
+sensorL = 16
+
+while True:
+  readingF = RPL.digitalRead(senorF)
+  trim = time.time() - theB
+  if trim > 20:
+     break
+  elif readingF == 0:
+     for number in list:
+        RPL.servoWrite(motorR, 1600)
+        time.sleep(1.5)
+        RPL.servowrite(motorR, 0)
+        time.sleep(1.5)
+  else:
+     RPL.servoWrite(motorR, 0) 
