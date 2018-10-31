f831d24d79550f6e86eb1a5c227093dbac79aadf
OSampson21 <44277509+OSampson21@users.noreply.github.com>
2018-10-31 09:48:09 -0500

    Create time.py

---
+++ time.py
@@ -0,0 +1,32 @@
+import RoboPiLip as RPL
+import time
+
+RPL.RoboPiInit("/dev/ttyAMA0",115200)
+
+motorR = 0
+motorL = 1
+AnalogSensor = 3
+sensorF = 17
+sensorL = 16
+
+while True:
+  readingF = RPL.digitalRead(senorF)
+  readingL = RPL.digitalRead(sensorL)
+  if readingF == 0 and readingL == 0:
+     break
+  elif readingF == 0:
+     RPL.servoWrite(motorL, 0)
+     RPL.servoWrite(motorR, 1600)
+     time.sleep(1.5)
+     RPL.servowrite(motorR, 1600)
+     time.sleep(1.5)
+    
+  elif readingL == 0:
+     RPL.servoWrite(motorR, 0)
+     RPL.servoWrite(motorL, 1600)
+     time.sleep(1)
+     RPL.servoWrite(motorL, 0)
+     time.sleep(1)
+    
+RPL.servoWrite(motorL,0)
+RPL.servoWrite(motorR,0)
