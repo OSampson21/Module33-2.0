cfcfa040d5255af0004e0d7bc67b50456f574ec1
OSampson21 <44277509+OSampson21@users.noreply.github.com>
2018-10-31 20:10:20 -0500

    Create print.py

---
+++ print.py
@@ -0,0 +1,21 @@
+import RoboPiLip as RPL
+import time
+
+RPL.RoboPiInit("/dev/ttyAMA0",115200)
+
+motorR = 0
+motorL = 1
+sensorF = 17
+sensorS = 16
+n = 0
+
+while True:
+  if RPL.digitalRead(sensorF) == 1:
+     RPL.servoWrite(motorR, 1600)
+     time.sleep(1)
+     RPL.servoWrite(motorR, 0)
+     time.sleep(1)
+     n += 1
+     print("The motor has pulsed " + n + " times.")
+   else:
+     pass
